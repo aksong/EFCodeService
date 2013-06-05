fb2a3024c493d854ce8fa54a762a27ec9081decf
Douglas Tarr <douglas.tarr@gmail.com>
2013-06-05 11:19:11 -0700

    initial commit

---
+++ EFCodeService/EFCodeService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Text.RegularExpressions;
+
+namespace EFCodeService
+{
+    public class EFCodeService
+    {
+        internal const int MaxStringLength = 10;
+        const int MAX_TRIES = 50;
+        DbContext _context;
+        int _codeLength;
+        public EFCodeService(DbContext context, int codeLength)
+        {
+            _context = context;
+            _codeLength = codeLength;
+        }
+
+        public string GenerateSlug(string title)
+        {
+            return title.GenerateSlug();
+        }
+
+        public string GenerateCode()
+        {
+            int counter = 0;
+            var dbset = _context.Set(typeof(EFCode));
+			while (true) {
+				var code = Utilities.GenerateRandomString(_codeLength);
+					dbset.Add(new EFCode { Code = code });
+					try {
+						_context.SaveChanges();
+                        return code;
+					} catch {
+                        if (counter++ > MAX_TRIES)
+                            return null;
+
+					}
+				}
+		}
+
+
+    }
+}
---
+++ EFCodeService/Utilities.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EFCodeService
+{
+    public static class Utilities
+    {
+        public static string GenerateRandomString(int size)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var stringChars = new char[size];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new String(stringChars);
+
+        }
+
+        public static string RemoveAccent(this string txt)
+        {
+            byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return System.Text.Encoding.ASCII.GetString(bytes);
+        }
+
+        public static string GenerateSlug(this string phrase)
+        {
+            string str = string.Empty;
+            try
+            {
+                str = phrase.RemoveAccent().ToLower();
+                // invalid chars           
+                str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+                // convert multiple spaces into one space   
+                str = Regex.Replace(str, @"\s+", " ").Trim();
+                // cut and trim 
+                str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+                str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            }
+            catch
+            {
+                str = "default";
+            }
+            return str;
+        }
+
+    }
+}
